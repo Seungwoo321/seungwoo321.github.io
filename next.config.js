14e92a8cde56585b6fa1550576f3d8a53d2cf006
Seungwoo321 <seungwoo321@gmail.com>
2022-10-30 18:03:12 +0900

    chore: remove options

--- next.config.js
+++ next.config.js
@@ -74,7 +74,7 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config, options) => {
+    webpack: (config) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
