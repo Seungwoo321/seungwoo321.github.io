0d64741ee9124dca52266caaf21ce40b94d9ed02
Seungwoo321 <seungwoo321@gmail.com>
2022-11-06 19:23:57 +0900

    chore: update script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git init
 git config --local user.name "Seungwoo Lee"
 git config --local user.email "seungwoo321@gmail.com"
 git add -A
-git commit -m "deploy by deploy.sh"
+git commit -m "chore: by deploy.sh"
 git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git master:gh-pages
 
 cd -
