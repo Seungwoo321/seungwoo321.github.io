47461056676090ef7b023e3e0c6f26fbb52844cd
Seungwoo321 <seungwoo321@gmail.com>
2022-11-06 19:22:10 +0900

    fix: resolve not working

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+cd out/
+
+git init 
+git config --local user.name "Seungwoo Lee"
+git config --local user.email "seungwoo321@gmail.com"
+git add -A
+git commit -m "deploy by deploy.sh"
+git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git master:gh-pages
+
+cd -
